28a51593bafdbb493f993702a6e1a124425db32f
default_git_name <default_git_email>
2016-05-02 00:19:32 +0000

    Rebuilt with PIE binaries for running on Lollipop or later

--- src/com/android/perl/config/GlobalConstants.java
+++ src/com/android/perl/config/GlobalConstants.java
@@ -27,10 +27,10 @@ public interface GlobalConstants {
 	public static final String PACKAGE_NAME = "com.android.perl";
 	
 	public static final String PERL_MAIN_SCRIPT_NAME = "l00httpd/00runmeAndroid.pl";
-	public static final String PERL_PROJECT_ZIP_NAME = "l00httpd_f911.cc398.zip";
+	public static final String PERL_PROJECT_ZIP_NAME = "wikipland_g501_cbefc.zip";
 	
 	public static final String PERL_NICE_NAME = "Perl 5.10.0";
-	public static final String PERL_ZIP_NAME = "perl_r9.zip";
+	public static final String PERL_ZIP_NAME = "perl_r10.zip";
 	public static final String PERL_EXTRAS_ZIP_NAME = "perl_extras_r7.zip";
 
 	public static final String LOG_TAG = "WikiPland";
